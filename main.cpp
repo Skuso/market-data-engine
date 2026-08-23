0155d87ecffcbfa7b2b037349d661d7366380239
Jerry <Jerryguerrero2121@gmail.com>
2026-08-23 02:25:39 -0500

    Modified main.cpp to attempt a TTLS handshake to coinbase

--- main.cpp
+++ main.cpp
@@ -1,10 +1,49 @@
 #include <format>
 #include <iostream>
-#include <boost/beast.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <string>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+
+using tcp = net::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
 
 int main() {
 
-    std::cout << std::format("Hello, {}!\n", "World");
-    
-    return 0;
+    // connect to: ws-feed.exchange.coinbase.com
+    // port: 443
+
+    std::cout << std::format("connecting to: {}:{}\n", "ws-feed.exchange.coinbase.com", 443);
+
+    try {
+        // The io_context is required for all I/O
+        net::io_context ioc;
+
+        // These objects perform our I/O
+        tcp::resolver resolver{ioc};
+        websocket::stream<tcp::socket> ws{ioc};
+
+        // Look up the domain name
+        auto const results = resolver.resolve("ws-feed.exchange.coinbase.com", "443");
+
+        // Make the connection on the IP address we get from a lookup
+        net::connect(ws.next_layer(), results.begin(), results.end());
+
+        // Perform the SSL handshake
+        ws.handshake("ws-feed.exchange.coinbase.com", "/");
+
+        std::cout << "connected\n";
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
