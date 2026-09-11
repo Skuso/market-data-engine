d839e3aa84f5158c51db5fe6bdc67e97d8888b0b
Jerry Guerrero <jerryguerrero2121@gmail.com>
2026-09-10 22:23:57 -0500

    using a pre-built skeleton for the order book class

---
+++ order-book.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "order-book.h"
+
+OrderBook::OrderBook() {
+    // Constructor implementation (if needed)
+}
+
+void OrderBook::apply_update(const nlohmann::json& message) {
+    
+}
